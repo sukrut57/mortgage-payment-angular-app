ec03485a3bc56d5010439cf89c0a49ddbecdeff8
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2024-11-20 14:55:19 -0500

    test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,7 @@ import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -25,7 +26,8 @@ import { MatButtonModule } from '@angular/material/button';
     ReactiveFormsModule,
     MatRadioGroup,
     MatRadioButton,
-    MatButtonModule
+    MatButtonModule,
+    CurrencyPipe 
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -33,6 +35,8 @@ import { MatButtonModule } from '@angular/material/button';
 export class HomeComponent implements OnInit {
   valid: any = true;
   mortgageForm:FormGroup;
+  monthlyPayment: number = 0;
+  totalPayment: number = 0;
 
   constructor(private _formBuilder: FormBuilder) {
     this.mortgageForm = this._formBuilder.group({
@@ -65,10 +69,95 @@ export class HomeComponent implements OnInit {
   }
 
   calculatePayments() {
-    console.log(this.mortgageForm.get('mortgageAmount')?.value);
-    console.log(this.mortgageForm.get('mortgageTerm')?.value);
-    console.log(this.mortgageForm.get('interestRate')?.value);
-    console.log(this.mortgageForm.get('mortgageType')?.value);
+    try{
+      if(this.mortgageForm.valid &&
+        this.mortgageAmount !== undefined &&
+        this.interestRate !== undefined &&
+        this.mortgageType !== undefined &&
+        this.mortgageTerm !== undefined
+      )
+      {
+        this.valid = true;
+  
+        console.log(this.mortgageAmount?.value);
+        console.log(this.interestRate?.value);
+        console.log(this.mortgageTerm?.value);
+        console.log(this.mortgageType?.value);
+
+      
+        if(this.mortgageType?.value === 'Repayment'){
+          this.calculateTotalRepayments();
+        }
+        else{
+          this.calculateTotalInterest();
+        }
+      }
+      else{
+        throw new Error('Invalid form');
+      }
+    }
+    catch(error){
+      console.error(error);
+    }
+  
+  }
+
+  //Getters
+  get mortgageAmount() {
+    return this.mortgageForm.get('mortgageAmount');
+  }
+  get mortgageTerm() {
+    return this.mortgageForm.get('mortgageTerm');
+  }
+
+  get interestRate() {
+    return this.mortgageForm.get('interestRate');
+  }
+
+  get mortgageType() {
+    return this.mortgageForm.get('mortgageType');
+  }
+
+  calculateTotalRepayments(){
+
+    const principal = this.mortgageAmount?.value;
+
+    const annualInterestRate = this.interestRate?.value / 100;
+
+    const monthlyInterestRate = annualInterestRate / 12;
+
+    const numberOfPayments = this.mortgageTerm?.value * 12;
+
+    const monthlyPayment = (principal * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
+
+    const totalPayment = monthlyPayment * numberOfPayments;
+
+    this.monthlyPayment = monthlyPayment;
+
+    this.totalPayment = totalPayment;
+
+  }
+
+
+  calculateTotalInterest(){
+    const principal = this.mortgageAmount?.value;
+
+    const annualInterestRate = this.interestRate?.value / 100;
+
+    const monthlyInterestRate = annualInterestRate / 12;
+
+    const numberOfPayments = this.mortgageTerm?.value * 12;
+
+    const totalInterest = principal * monthlyInterestRate * numberOfPayments;
+
+    this.totalPayment = totalInterest;
+  }
+
+  clearForm(){
+    this.mortgageForm.reset();
+    this.valid = false;
+    this.monthlyPayment = 0;
+    this.totalPayment = 0;
 
   }
 }
